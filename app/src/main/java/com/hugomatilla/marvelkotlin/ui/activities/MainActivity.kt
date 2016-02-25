15bd338b6a70a8303e60eabf107fb4f647c582e4
HugoMatilla <hugomatilla@gmail.com>
2016-02-25 19:45:48 +0100

    Add Toolbar

--- app/src/main/java/com/hugomatilla/marvelkotlin/ui/activities/MainActivity.kt
+++ app/src/main/java/com/hugomatilla/marvelkotlin/ui/activities/MainActivity.kt
@@ -3,25 +3,28 @@ package com.hugomatilla.marvelkotlin.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import com.hugomatilla.marvelkotlin.R
 import com.hugomatilla.marvelkotlin.domain.RequestHeroUseCase
 import com.hugomatilla.marvelkotlin.domain.RequestHeroesUseCase
 import com.hugomatilla.marvelkotlin.ui.adapters.HeroesListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.async
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarManager {
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     private var syncFinished: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+
 
         heroesListView.layoutManager = LinearLayoutManager(this)
+        attachToScroll(heroesListView)
+        toolbarTitle = "Marvel"
 
         async() {
             val result = RequestHeroesUseCase().execute()
---
+++ app/src/main/java/com/hugomatilla/marvelkotlin/ui/activities/ToolbarManager.kt
@@ -0,0 +1,54 @@
+package com.hugomatilla.marvelkotlin.ui.activities
+
+import android.support.v7.graphics.drawable.DrawerArrowDrawable
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import com.hugomatilla.marvelkotlin.R
+import com.hugomatilla.marvelkotlin.ui.App
+import com.hugomatilla.marvelkotlin.ui.extensions.ctx
+import com.hugomatilla.marvelkotlin.ui.extensions.slideEnter
+import com.hugomatilla.marvelkotlin.ui.extensions.slideExit
+import org.jetbrains.anko.toast
+
+/**
+ * Created by hugomatilla on 25/02/16.
+ */
+interface ToolbarManager {
+
+    val toolbar: Toolbar
+
+    var toolbarTitle: String
+        get() = toolbar.title.toString()
+        set(value) {
+            toolbar.title = value
+        }
+
+    fun initToolbar() {
+        toolbar.inflateMenu(R.menu.menu_main)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_settings -> App.instance.toast("Settings")
+                else -> App.instance.toast("Unknown option")
+            }
+            true
+        }
+    }
+
+    fun enableHomeAsUp(up: () -> Unit) {
+        toolbar.navigationIcon = createUpDrawable()
+        toolbar.setNavigationOnClickListener { up() }
+    }
+
+    private fun createUpDrawable() = with (DrawerArrowDrawable(toolbar.ctx)) {
+        progress = 1f
+        this
+    }
+
+    fun attachToScroll(recyclerView: RecyclerView) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                if (dy > 0) toolbar.slideExit() else toolbar.slideEnter()
+            }
+        })
+    }
+}
